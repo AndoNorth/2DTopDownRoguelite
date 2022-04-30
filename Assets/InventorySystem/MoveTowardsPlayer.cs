286ba27380cbb437600dd3e5ce56ec84e37515ab
Ando <55718737+AndoNorth@users.noreply.github.com>
2022-04-30 14:39:54 +0100

    Update MoveTowardsPlayer.cs
    1) updated move towards player to have magnetic effect when within certain range

--- Assets/InventorySystem/MoveTowardsPlayer.cs
+++ Assets/InventorySystem/MoveTowardsPlayer.cs
@@ -6,11 +6,13 @@ public class MoveTowardsPlayer : MonoBehaviour
     [SerializeField] private float _acceleration;
     [SerializeField] private float _maxSpeed;
     private float _speed;
+    [SerializeField] private float _magnetRange;
+    private bool _targetInMagnetRange = false;
     [Header("UpdateTargetPositionSettings")]
     [SerializeField] private float _delayStartMovingSec;
     [SerializeField] private float _repeatEveryNSec;
     private Vector3 _targetPos;
-    private bool _targetSet;
+    private bool _targetSet = false;
     private void Start()
     {
         InvokeRepeating("UpdateTargetPosition", _delayStartMovingSec, _repeatEveryNSec);
@@ -21,7 +23,10 @@ public class MoveTowardsPlayer : MonoBehaviour
         UpdateSpeed();
         if (_targetSet)
         {
-            transform.position = Vector3.MoveTowards(transform.position, _targetPos, _speed * Time.deltaTime);
+            if (_targetInMagnetRange)
+                transform.position = _targetPos;
+            else
+                transform.position = Vector3.MoveTowards(transform.position, _targetPos, _speed * Time.deltaTime);
         }
     }
     private void OnDisable()
@@ -38,6 +43,8 @@ public class MoveTowardsPlayer : MonoBehaviour
     {
         if (!_targetSet)
             _targetSet = true;
+        if (Vector3.Distance(transform.position, GameAssets.instance.playerCharacter.transform.position) <= _magnetRange)
+            _targetInMagnetRange = true;
         _targetPos = GameAssets.instance.playerCharacter.transform.position;
     }
 }
