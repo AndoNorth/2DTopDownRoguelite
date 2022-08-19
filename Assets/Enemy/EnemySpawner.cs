a9edab203f7276ff9df82c70d7d144e2f82c7034
Ando <55718737+AndoNorth@users.noreply.github.com>
2022-08-19 17:00:49 +0100

    EnemySpawner
    1) updated Enemy Spawner scripts to have delay before spawning, and initializing Enemies
    2) rebalanced Ranged and Melee spawners

--- Assets/Enemy/EnemySpawner.cs
+++ Assets/Enemy/EnemySpawner.cs
@@ -1,40 +1,33 @@
 ﻿using UnityEngine;
-// kieran wrote this
 public class EnemySpawner : MonoBehaviour
 {
-    //Public Variables
-    public GameObject enemy;
+    [SerializeField] private GameObject pEnemy;
+    [Header("Spawner Settings")]
+    [SerializeField] private float spawnTime;
+    [SerializeField] private int maxNoEnemies = 3;
+    [SerializeField] private bool randomizeSpawnTime = false;
+    [SerializeField] private float minRandomRange = 0.0f;
+    [SerializeField] private float maxRandomRange = 1.0f;
 
-    public float spawnTime; // maybe change to per second then calculate the spawnRate in start function
-    public float spawnTimeRandom; // then we can make this a random scaled version of that
-    public GameObject target;
-    public int maxNoEnemies = 3;
-
-    //Private Variables
     private float spawnTimer;
     
-    //Used for initialisation
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Character");
-        ResetSpawnTimer();
+        spawnTimer = 10.0f;
     }
 
-    //Update is called once per frame
     void Update()
+    {
+        Spawn();
+    }
+    void Spawn()
     {
         spawnTimer -= Time.deltaTime;
         if (spawnTimer <= 0.0f && this.transform.childCount < maxNoEnemies)
         {
-            GameObject enemyGO = Instantiate(enemy, transform.position, Quaternion.identity);
+            GameObject enemyGO = Instantiate(pEnemy, transform.position, Quaternion.identity);
             enemyGO.transform.SetParent(this.transform);
-            ResetSpawnTimer();
+            spawnTimer = randomizeSpawnTime ? Random.Range(minRandomRange, maxRandomRange) : spawnTime;
         }
-    }
-
-    //Resets the spawn timer with a random offset
-    void ResetSpawnTimer()
-    {
-        spawnTimer = (float)(spawnTime + Random.Range(0, spawnTimeRandom * 100) / 100.0);
     }
 }
